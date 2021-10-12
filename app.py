bb332a7fca8ec0137325b0ec3c48aac9df24aaa6
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-12 23:29:02 +0200

    Cosmetic improvements

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print("This is a simple square root calculator.\nPlease provide the following information...")
-number = float(input("Radicant: "))
-precision = float(input("Precision: "))
+number = float(input(" - radicant: "))
+precision = float(input(" - precision: "))
 result = number / 2 # Initial guess
 
 while abs(result ** 2 - number) > precision:
   result = (result + number / result) / 2
 
-print(f"...the square root of {number} is {result}.")
+print(f"...the square root of {number} is ~{result}.")
