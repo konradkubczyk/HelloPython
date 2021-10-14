caf27af77678386401853f67ca4ac7aeb80b53b9
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-14 12:39:42 +0200

    Added exercise 16

---
+++ Types and Variables/ex16.py
@@ -0,0 +1,25 @@
+# 16. Evaluate the following expressions.
+
+# a. The product of 15 and 38
+print(15 * 38)
+
+# b. The product of the sum of the pairs of numbers 3 and 4, and 5 and 9
+print((3 + 4) * (5 * 9))
+
+# c. Integer part of dividing the numbers 7 and 2
+print(7 // 2)
+
+# d. The remainder of the division of 48 and 5
+print(48 % 5)
+
+# e. Arithmetic mean of the numbers 8, 7, 4, 2
+print((8 + 7 + 4 + 2) / 4)
+
+# f. 2^10
+print(2 ** 10)
+
+# g. Square root of 49 (do not use a function)
+print(49 ** 0.5)
+
+# h. 25% of 80
+print(0.25 * 80)
