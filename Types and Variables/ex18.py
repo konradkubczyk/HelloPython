c7bcfa34db82219da4edd435bceb89826cc3f9e3
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-14 13:24:29 +0200

    Added exercise 18

---
+++ Types and Variables/ex18.py
@@ -0,0 +1,6 @@
+# 18. In interactive mode, calculate and display your height in feet and inches.
+heightCm = float(input("Please enter your height in cm: "))
+heightIn = round(heightCm / 2.54, 0)
+heightIntegralFt = heightIn // 12
+heightRemainingIn = heightIn - heightIntegralFt * 12
+print(f"Your height of {heightCm} cm is approximately equal to {int(heightIntegralFt)} feet and {int(round(heightRemainingIn, 0))} inches.")
