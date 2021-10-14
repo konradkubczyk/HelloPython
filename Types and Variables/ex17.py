b82817cf82b7769a8c0021b38ffd97fab1b3f43f
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-14 12:48:47 +0200

    Added exercise 17

---
+++ Types and Variables/ex17.py
@@ -0,0 +1,46 @@
+# 17. What are the values of the following expressions? Answer without using the computer. Then check the results on your computer, in interactive mode.
+
+# a. 5 + 10 * 5
+print(5 + 10 * 5)
+
+# b. 3 - 2 + 1
+print(3 - 2 + 1)
+
+# c. 2 + - 3
+print(2 + - 3)
+
+# d. 2 ** 8
+print(2 ** 8)
+
+# e. 4 + 4 / 2 ** 2
+print(4 + 4 / 2 ** 2)
+
+# f. 4 % 3 % 2 % 1
+print(4 % 3 % 2 % 1)
+
+# g. 1 + 2 % 3 ** 4 * 5
+print(1 + 2 % 3 ** 4 * 5)
+
+# h. True != False
+print(True != False)
+
+# i. 2 <= 3 or False
+print(2 <= 3 or False)
+
+# j. not True or not False and not True
+print(not True or not False and not True)
+
+# k. 2 < 3 and 4 < 5 or not 6 < 7
+print(2 < 3 and 4 < 5 or not 6 < 7)
+
+# l. 2 % 3 < 4 / 5 and 6 + 7 < 8 or not 9 + 10 == 19
+print(2 % 3 < 4 / 5 and 6 + 7 < 8 or not 9 + 10 == 19)
+
+# m. 0b11111 >> 1 >> 1 >> 1
+print(0b11111 >> 1 >> 1 >> 1)
+
+# n. 0x11 + 0b11 + 11
+print(0x11 + 0b11 + 11)
+
+# o. 2 << 3 >> 4
+print(2 << 3 >> 4)
