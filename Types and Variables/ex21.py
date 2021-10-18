914acfa5dae8934052f0185e596ff424ae261b7b
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-18 20:11:14 +0200

    Added exercise 21

---
+++ Types and Variables/ex21.py
@@ -0,0 +1,8 @@
+import random
+
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+dice3 = random.randint(1, 6)
+sum = dice1 + dice2 + dice3
+
+print(f"First dice roll:\t{dice1}\nSecond dice roll:\t{dice2}\nThird dice roll:\t{dice3}\nSum of the results:\t{sum}")
