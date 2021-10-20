98db5b483839b759b1f0342746b5758aadccac8e
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-20 13:56:31 +0200

    Added an example of an iteration over a string

--- Control Structures/intro.py
+++ Control Structures/intro.py
@@ -5,7 +5,12 @@ nOfAttempts = 5
 for number in range(1, nOfAttempts + 1):
     print("Attempt", number)
     if successfull:
-        print("Success! ^^")
+        print("\nSuccess! ^^")
         break
 else:
-    print(f"Attempted {nOfAttempts} times and failed. :(")
+    print(f"\nAttempted {nOfAttempts} times and failed. :(")
+
+print("")
+
+for character in "Python":
+    print(character)
