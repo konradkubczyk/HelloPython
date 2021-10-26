8bdf76deb84ffeb4efa6cdacaee3368413c67159
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-26 22:23:30 +0200

    Added exercise 20

---
+++ Control Structures/20.py
@@ -0,0 +1,6 @@
+# Write a program that creates a multiplication table in the range 1 to 10 for any number entered by the user.
+
+number = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
