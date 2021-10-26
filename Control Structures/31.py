6d785be74fe526e65e171367920dae871982fe84
Konrad Kubczyk <konrad@kubczyk.com>
2021-10-26 23:37:12 +0200

    Added exercise 31

---
+++ Control Structures/31.py
@@ -0,0 +1,8 @@
+# Write a program that displays 20 integer random numbers in the range of 5 to 10.
+
+import random
+
+for i in range(20):
+    print(random.randrange(5, 11), end = " ")
+
+print("")
