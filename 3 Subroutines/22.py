6856ffa7aac535d4ec3898efc13c96ec26c20207
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-02 20:23:38 +0100

    Added exercise 22

---
+++ 3 Subroutines/22.py
@@ -0,0 +1,6 @@
+# Define an anonymous function that returns true when the number is even.
+
+is_even = lambda number: number % 2 == 0
+
+print('Is 2 even?', is_even(2))
+print('Is 7 even?', is_even(7))
