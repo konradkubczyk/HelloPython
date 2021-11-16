a955fe5a8617de9a8cd58a0e31209cbd3fbff772
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-16 20:28:34 +0100

    Added exercise 15

---
+++ 4 Arrays/15/15.py
@@ -0,0 +1,10 @@
+# An array contains color names. Create a program that writes the contents of an array to a text file. Put each color on a separate line.
+
+colors = ["yellow", "green", "blue", "purple", "red", "orange"]
+
+output_file = open("15.txt", "w")
+
+for color in colors:
+    output_file.write(color + "\n")
+
+output_file.close()
