1b5b20415a8b567ce64b132da30be5b811b4701e
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-16 21:44:26 +0100

    Added exercise 19

---
+++ 4 Arrays/19.py
@@ -0,0 +1,31 @@
+# Create a program that displays all unique elements in an array. Sample result:
+
+# Array: 2 3 2 5 8 1 9 8 Unique elements: 3 5 1 9
+
+array = [2, 3, 2, 5, 8, 1, 9, 8]
+
+unique_elements = []
+
+for selected_number in array:
+    occurences = 0
+    for number in array:
+        if number == selected_number:
+            occurences += 1
+            if occurences > 1:
+                break
+    else:
+        unique_elements.append(selected_number)
+
+print("Array: ", end="")
+
+for n in array:
+    print(n, end=" ")
+
+print()
+
+print("Unique elements: ", end="")
+
+for n in unique_elements:
+    print(n, end=" ")
+
+print()
