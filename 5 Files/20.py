389d2a500a9ef96b680401de2ed15c1e135658cd
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-22 21:19:54 +0100

    File extension fix.

--- 5 Files/20.py
+++ 5 Files/20.py
@@ -1,6 +1,6 @@
 # Create a program that writes to a text file integer numbers in the range of <1,50>, every number in a separate line.
 
-with open("out_20.py", "w") as file:
+with open("20.txt", "w") as file:
     for n in range(50):
         file.write(str(n + 1) + "\n")
     print("File written successfully.")
