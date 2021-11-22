c5cdddd74cc5f25f4a756257af99484f1e3cdfc3
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-22 22:10:52 +0100

    Description fix

--- 4 Arrays/19.py
+++ 4 Arrays/19.py
@@ -1,6 +1,7 @@
 # Create a program that displays all unique elements in an array. Sample result:
 
-# Array: 2 3 2 5 8 1 9 8 Unique elements: 3 5 1 9
+# Array: 2 3 2 5 8 1 9 8
+# Unique elements: 3 5 1 9
 
 array = [2, 3, 2, 5, 8, 1, 9, 8]
 
