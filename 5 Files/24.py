6f6c76efff3b10a8871e2228176e79de52f2bbee
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-22 22:34:52 +0100

    Description readability fix

---
+++ 5 Files/24.py
@@ -0,0 +1,13 @@
+# The announcement regarding the temperature forecast in degrees Celsius for the next three days was published on the Internet:
+#
+# "Tuesday: 22C, Wednesday: 21C, Thursday: 26C "
+#
+# Create a program that calculates the average temperature. Use regular expressions to extract the values of temperatures from the message.
+#
+# import re message = "Tuesday: 22C, Wednesday: 21C, Thursday: 26C "
+# temperatures = re.findall("\d{2}",message)
+# # complete the program code
+
+import re message = "Tuesday: 22C, Wednesday: 21C, Thursday: 26C "
+temperatures = re.findall("\d{2}",message)
+# complete the program code
