928716ab6781cfaa5085c48e39a7a0100a5c5623
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-22 22:35:54 +0100

    Added exercise 24

--- 5 Files/24.py
+++ 5 Files/24.py
@@ -8,6 +8,6 @@
 # temperatures = re.findall("\d{2}",message)
 # # complete the program code
 
-import re message = "Tuesday: 22C, Wednesday: 21C, Thursday: 26C "
+import re
+message = "Tuesday: 22C, Wednesday: 21C, Thursday: 26C "
 temperatures = re.findall("\d{2}",message)
-# complete the program code
